0fd144d373d7862e5212c220da7b6bbd1fd8793a
GalinaNetishina <netishinagalina@yandex.ru>
2024-02-08 17:08:49 +0300

    rework

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -37,7 +37,7 @@ def test_create(client, student_factory):
         'name': 'python',
         'students': [person.id for person in studs]
     }
-    response = client.post('http://127.0.0.1:8000/api/v1/courses/', data)
+    response = client.post('/api/v1/courses/', data)
 
     assert response.status_code == 201
     assert Course.objects.count() == count + 1
@@ -47,7 +47,7 @@ def test_create(client, student_factory):
 def test_retrieve(client, course_factory):
     course = course_factory(_quantity=1)
 
-    response = client.get(f'http://127.0.0.1:8000/api/v1/courses/{course[0].id}/')
+    response = client.get(f'/api/v1/courses/{course[0].id}/')
     data = response.json()
 
     assert response.status_code == 200
@@ -59,7 +59,7 @@ def test_delete(client, course_factory):
     _course = course_factory(_quantity=1)
     count = Course.objects.count()
 
-    response = client.delete(f'http://127.0.0.1:8000/api/v1/courses/{_course[0].id}/')
+    response = client.delete(f'/api/v1/courses/{_course[0].id}/')
 
     assert response.status_code == 204
     assert Course.objects.count() == count - 1
@@ -70,9 +70,9 @@ def test_update(client, course_factory, student_factory):
     studs = student_factory(_quantity=3)
     _course = course_factory(_quantity=1, students=[studs[0]])
     count = Course.objects.count()
-    data = {"students": [1, 2]}
+    data = {"students": [studs[0].id, studs[2].id]}
 
-    response = client.patch(f'http://127.0.0.1:8000/api/v1/courses/{_course[0].id}/', data)
+    response = client.patch(f'/api/v1/courses/{_course[0].id}/', data)
     res = response.json()
 
     assert response.status_code == 200
@@ -85,7 +85,7 @@ def test_list_retrieve(client, course_factory, student_factory):
     students_set = baker.prepare(Student, _quantity=3)
     _courses = course_factory(_quantity=3, students=students_set)
 
-    response = client.get('http://127.0.0.1:8000/api/v1/courses/')
+    response = client.get('/api/v1/courses/')
     data = response.json()
 
     assert response.status_code == 200
@@ -100,7 +100,7 @@ def test_filter_name(client, course_factory, student_factory):
     students_set = baker.prepare(Student, _quantity=16)
     _courses = course_factory(_quantity=8, students=sample(students_set, 2))
     target = choice(_courses)
-    response = client.get(f'http://127.0.0.1:8000/api/v1/courses/?name={target.name}/')
+    response = client.get(f'/api/v1/courses/?name={target.name}/')
     data = response.json()
 
     assert response.status_code == 200
@@ -113,9 +113,10 @@ def test_filter_id(client, course_factory, student_factory):
     students_set = baker.prepare(Student, _quantity=10)
     _courses = course_factory(_quantity=8, students=sample(students_set, 2))
     target = choice(_courses)
-    response = client.get(f'http://127.0.0.1:8000/api/v1/courses/?id={target.id}')
+    response = client.get(f'/api/v1/courses/?id={target.id}')
     data = response.json()
 
     assert response.status_code == 200
     assert len(data) == 1
     assert data[0]['id'] == target.id
+
